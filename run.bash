ff4f106a531438554570aa93777b9a8313fbe8d2
gongyixiao <gongyixiao@users.noreply.github.com>
2019-03-08 18:32:37 -0500

    Important outDir parameter added for AWS-batch run

--- run.bash
+++ run.bash
@@ -3,4 +3,5 @@
 sample=$1
 work_dir="$2"
 awsqueue="$3"
-nextflow run main.nf -with-report report.html --sample $sample --genome GRCh37 --genome_base="s3://sarek-igenomes/GRCh37/" --step mapping -resume --tag latest -profile awsbatch -w "$2" --awsqueue "$3"
+output_dir="$4"
+nextflow run main.nf -with-report report.html --sample $sample --genome GRCh37 --genome_base="s3://sarek-igenomes/GRCh37/" --step mapping -resume --tag latest -profile awsbatch -w "$2" --awsqueue "$3" --outDir "$4"
